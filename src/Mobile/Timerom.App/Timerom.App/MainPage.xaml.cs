fc6bcac7a595563726b364d00bc6794f4eb8f172
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-08-09 14:21:10 +0100

    Initial app setup

---
+++ src/Mobile/Timerom.App/Timerom.App/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Timerom.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
