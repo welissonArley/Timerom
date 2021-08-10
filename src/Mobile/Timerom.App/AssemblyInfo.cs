d006f358cd7a840d132486300ffbe988aa8fdea7
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-08-10 17:58:40 +0100

    Adding fonts

--- src/Mobile/Timerom.App/AssemblyInfo.cs
+++ src/Mobile/Timerom.App/AssemblyInfo.cs
@@ -1,3 +1,9 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Raleway-Bold.ttf", Alias = "RalewayBold")]
+[assembly: ExportFont("Raleway-ExtraLight.ttf", Alias = "RalewayExtraLight")]
+[assembly: ExportFont("Raleway-SemiBold.ttf", Alias = "RalewaySemiBold")]
+[assembly: ExportFont("Raleway-Regular.ttf", Alias = "RalewayRegular")]
