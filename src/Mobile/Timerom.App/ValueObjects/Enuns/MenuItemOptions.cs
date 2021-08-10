5bc5c3a51670ba737017c2d6078f5515b375dfb3
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-08-10 18:00:38 +0100

    Sidebar menu ok

---
+++ src/Mobile/Timerom.App/ValueObjects/Enuns/MenuItemOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Timerom.App.ValueObjects.Enuns
+{
+    public enum MenuItemOptions
+    {
+        Dashboard = 0,
+        Categories = 1,
+        PrivacyPolicy = 2,
+        UseTerms = 3,
+        IconsAndIllustrations = 4,
+        ContactUs = 5
+    }
+}
---
+++ src/Mobile/Timerom.App/Views/Home/HomePageDetail.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Timerom.App.Views.Home
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePageDetail : ContentPage
+    {
+        public HomePageDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
