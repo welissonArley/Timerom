5208dc318ef61d13518c25401d2afeefe8a82d22
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-08-11 09:28:58 +0100

    Page to show icons and illustrations information working

---
+++ src/Mobile/Timerom.App/ViewModels/Home/HomePageFlyoutViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Timerom.App.ValueObjects.Enuns;
+
+namespace Timerom.App.ViewModels.Home
+{
+    public class HomePageFlyoutViewModel
+    {
+        public MenuItemOptions? MenuItemSelected { get; set; } = null;
+
+        public HomePageFlyoutViewModel(MenuItemOptions selected)
+        {
+            MenuItemSelected = selected;
+        }
+    }
+}
