a479d22cf58ba121b9987601396c1ee7981e8d7b
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-08-17 08:26:25 +0100

    database to manage the categories

---
+++ src/Mobile/Timerom.App/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Timerom.App.ValueObjects.Enuns;
+
+namespace Timerom.App.Model
+{
+    public class Category
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public CategoryType Type { get; set; }
+        public IList<Category> Childrens { get; set; }
+    }
+}
---
+++ src/Mobile/Timerom.App/ValueObjects/Entity/Category.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using Timerom.App.ValueObjects.Enuns;
+
+namespace Timerom.App.ValueObjects.Entity
+{
+    public class Category
+    {
+        [PrimaryKey, AutoIncrement]
+        public long Id { get; set; }
+        [NotNull]
+        public string Name { get; set; }
+        [NotNull]
+        public CategoryType Type { get; set; }
+        public long? ParentCategoryId { get; set; }
+    }
+}
