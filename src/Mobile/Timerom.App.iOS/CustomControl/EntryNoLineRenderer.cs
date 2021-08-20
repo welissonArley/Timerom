3ad4d6af637b01fa0454856b48b0464ffc38ea9c
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-08-20 14:08:28 +0100

    Custom entry to dont show line

---
+++ src/Mobile/Timerom.App.iOS/CustomControl/EntryNoLineRenderer.cs
@@ -0,0 +1,44 @@
+﻿using CoreAnimation;
+using CoreGraphics;
+using Timerom.App.CustomControl;
+using Timerom.App.iOS.CustomControl;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(EntryNoLine), typeof(EntryNoLineRenderer))]
+namespace Timerom.App.iOS.CustomControl
+{
+    public class EntryNoLineRenderer : EntryRenderer
+    {
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+			if (Control == null || e.NewElement == null)
+				return;
+
+			Control.BorderStyle = UITextBorderStyle.None;
+
+			CALayer _line = new CALayer
+			{
+				BorderColor = GetLineColor(),
+				BackgroundColor = GetLineColor(),
+				Frame = new CGRect(0, Frame.Height / 2, UIScreen.MainScreen.Bounds.Width - 40, 1f)
+			};
+
+			Control.Layer.AddSublayer(_line);
+			Control.TintColor = GetCursor();
+		}
+
+		private CGColor GetLineColor()
+		{
+			return Xamarin.Forms.Application.Current.RequestedTheme == OSAppTheme.Dark ? UIColor.White.CGColor :
+				UIColor.FromRGB(31, 30, 25).CGColor;
+		}
+
+		private UIColor GetCursor()
+		{
+			return Xamarin.Forms.Application.Current.RequestedTheme == OSAppTheme.Dark ? UIColor.White : UIColor.Black;
+		}
+	}
+}
