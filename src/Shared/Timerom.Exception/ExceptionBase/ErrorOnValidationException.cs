222ec7de7a38960c5e5ec4af8bcac52f9d66c7d8
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-08-24 09:46:32 +0100

    Adding exception project

---
+++ src/Shared/Timerom.Exception/ExceptionBase/ErrorOnValidationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Timerom.Exception.ExceptionBase
+{
+    [Serializable]
+    public class ErrorOnValidationException : TimeromException
+    {
+        public List<string> ErrorMensages { get; set; }
+        public ErrorOnValidationException(List<string> listErrors) : base("")
+        {
+            ErrorMensages = listErrors;
+        }
+
+        protected ErrorOnValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/Shared/Timerom.Exception/ExceptionBase/TimeromException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Timerom.Exception.ExceptionBase
+{
+    [Serializable]
+    public class TimeromException : SystemException
+    {
+        protected TimeromException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public TimeromException(string mensagem) : base(mensagem)
+        {
+        }
+    }
+}
