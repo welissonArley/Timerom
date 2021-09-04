5efea9cf8a55be093d81e8406f4fd5fec5cc7c67
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-09-04 21:17:50 +0100

    feat(Templates): adding Text area template

---
+++ src/Mobile/Timerom.App/Views/Templates/Input/TextAreaTemplate.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Timerom.App.Views.Templates.Input
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TextAreaTemplate : ContentView
+    {
+        public string PlaceHolderText { set => Input.Placeholder = value; }
+
+        public object BindingEntry
+        {
+            get => GetValue(BindindEntryProperty);
+            set => SetValue(BindindEntryProperty, value);
+        }
+        public static readonly BindableProperty BindindEntryProperty = BindableProperty.Create(
+                                                        propertyName: "BindingEntry",
+                                                        returnType: typeof(object),
+                                                        declaringType: typeof(TextAreaTemplate),
+                                                        defaultValue: null,
+                                                        defaultBindingMode: BindingMode.TwoWay,
+                                                        propertyChanged: BindindEntryChanged);
+
+        private static void BindindEntryChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var bindableComponent = (TextAreaTemplate)bindable;
+            bindableComponent.Input.SetBinding(Entry.TextProperty, (Binding)newValue);
+        }
+
+        public TextAreaTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
