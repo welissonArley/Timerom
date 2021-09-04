0c52189fe8157bb596420e0d96bfcaf8b403b308
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-09-04 21:24:50 +0100

    feat(Page): Navigate to add task page

--- src/Mobile/Timerom.App/ViewModels/Tasks/SelectSubCategoryForTaskViewModel.cs
+++ src/Mobile/Timerom.App/ViewModels/Tasks/SelectSubCategoryForTaskViewModel.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
+using Timerom.App.Model;
+using Timerom.App.Views.Views.Tasks;
 using Xamarin.CommunityToolkit.ObjectModel;
 
 namespace Timerom.App.ViewModels.Tasks
@@ -10,6 +12,7 @@ namespace Timerom.App.ViewModels.Tasks
     public class SelectSubCategoryForTaskViewModel : ViewModelBase, IInitializeAsync
     {
         public IAsyncCommand<string> SearchTextChangedCommand { get; private set; }
+        public IAsyncCommand<Model.Category> ItemSelectedCommand { get; private set; }
 
         private ObservableCollection<Model.Category> _subCategories { get; set; }
         public Model.Category Category { get; private set; }
@@ -17,6 +20,7 @@ namespace Timerom.App.ViewModels.Tasks
         public SelectSubCategoryForTaskViewModel(Lazy<INavigationService> navigationService) : base(navigationService)
         {
             SearchTextChangedCommand = new AsyncCommand<string>(OnSearchTextChanged);
+            ItemSelectedCommand = new AsyncCommand<Model.Category>(ItemSelectedCommandExecuted, allowsMultipleExecutions: false);
         }
 
         private Task OnSearchTextChanged(string value)
@@ -26,6 +30,16 @@ namespace Timerom.App.ViewModels.Tasks
             RaisePropertyChanged("Category");
 
             return Task.CompletedTask;
+        }
+
+        private async Task ItemSelectedCommandExecuted(Model.Category category)
+        {
+            var navParameters = new NavigationParameters
+            {
+                { "Task", new TaskModel { Category = category, StartAt = DateTime.Now, EndAt = DateTime.Now.AddHours(1) } }
+            };
+
+            await _navigationService.NavigateAsync(nameof(AddUpdateTaskPage), navParameters);
         }
 
         public Task InitializeAsync(INavigationParameters parameters)
