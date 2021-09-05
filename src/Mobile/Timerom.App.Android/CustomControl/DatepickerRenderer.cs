c576a25af797b4d3e4cd6e82996a363b4d1c318b
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-09-05 12:22:16 +0100

    feat(Datepicker): adding datepicker native in the project

---
+++ src/Mobile/Timerom.App.Android/CustomControl/DatepickerRenderer.cs
@@ -0,0 +1,20 @@
+﻿using Timerom.App.Droid.CustomControl;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(DatepickerRenderer))]
+namespace Timerom.App.Droid.CustomControl
+{
+    public class DatepickerRenderer : DatePickerRenderer
+    {
+        public DatepickerRenderer(Android.Content.Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+                Control.SetBackgroundResource(0);
+        }
+    }
+}
--- src/Mobile/Timerom.App/ViewModels/Tasks/AddUpdateTaskViewModel.cs
+++ src/Mobile/Timerom.App/ViewModels/Tasks/AddUpdateTaskViewModel.cs
@@ -9,17 +9,27 @@ namespace Timerom.App.ViewModels.Tasks
         public TaskModel Task { get; set; }
         public TimeSpan TotalTime => Task == null ? new TimeSpan() : (Task.EndsAt - Task.StartsAt);
 
-        public TimeSpan StartsAt
+        public TimeSpan TimeStartsAt
         {
             get => Task == null ? new TimeSpan() : new TimeSpan(Task.StartsAt.Hour, Task.StartsAt.Minute, 0);
             set { Task.StartsAt = Task.StartsAt.Date + value; RaisePropertyChanged("TotalTime"); }
         }
-        public TimeSpan EndsAt
+        public TimeSpan TimeEndsAt
         {
             get => Task == null ? new TimeSpan() : new TimeSpan(Task.EndsAt.Hour, Task.EndsAt.Minute, 0);
             set { Task.EndsAt = Task.EndsAt.Date + value; RaisePropertyChanged("TotalTime"); }
         }
-        
+        public DateTime DateStartsAt
+        {
+            get => Task == null ? DateTime.Now : new DateTime(Task.StartsAt.Year, Task.StartsAt.Month, Task.StartsAt.Day);
+            set { Task.StartsAt = value.Date + TimeStartsAt; RaisePropertyChanged("TotalTime"); RaisePropertyChanged("DateStartsAt"); RaisePropertyChanged("DateEndsAt"); }
+        }
+        public DateTime DateEndsAt
+        {
+            get => Task == null ? DateTime.Now : new DateTime(Task.EndsAt.Year, Task.EndsAt.Month, Task.EndsAt.Day);
+            set { Task.EndsAt = value.Date + TimeEndsAt; RaisePropertyChanged("TotalTime"); RaisePropertyChanged("DateStartsAt"); RaisePropertyChanged("DateEndsAt"); }
+        }
+
         public AddUpdateTaskViewModel(Lazy<INavigationService> navigationService) : base(navigationService)
         {
         }
@@ -39,8 +49,10 @@ namespace Timerom.App.ViewModels.Tasks
             };
             RaisePropertyChanged("Task");
             RaisePropertyChanged("TotalTime");
-            RaisePropertyChanged("StartsAt");
-            RaisePropertyChanged("EndsAt");
+            RaisePropertyChanged("TimeStartsAt");
+            RaisePropertyChanged("TimeEndsAt");
+            RaisePropertyChanged("DateStartsAt");
+            RaisePropertyChanged("DateEndsAt");
         }
     }
 }
