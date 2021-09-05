bb1d48f21b0dd60b68a743862b5c46a575a4e953
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-09-05 14:49:37 +0100

    feat(Database): Database to add user tasks

---
+++ src/Mobile/Timerom.App/Repository/UserTaskDatabase.cs
@@ -0,0 +1,35 @@
+﻿using SQLite;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Timerom.App.ValueObjects.Entity;
+
+namespace Timerom.App.Repository
+{
+    public class UserTaskDatabase
+    {
+        private static SQLiteAsyncConnection _database;
+        private static readonly AsyncLazy<UserTaskDatabase> _instance = new AsyncLazy<UserTaskDatabase>(async () =>
+        {
+            UserTaskDatabase instance = new UserTaskDatabase();
+            CreateTableResult result = await _database.CreateTableAsync<UserTask>();
+
+            return instance;
+        });
+
+        public UserTaskDatabase()
+        {
+            _database = new SQLiteAsyncConnection(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "timerom.db3"));
+        }
+
+        public static async Task<UserTaskDatabase> Instance()
+        {
+            return await _instance;
+        }
+
+        public async Task Save(UserTask task)
+        {
+            _ = await _database.InsertAsync(task);
+        }
+    }
+}
---
+++ src/Mobile/Timerom.App/ValueObjects/Entity/UserTask.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+
+namespace Timerom.App.ValueObjects.Entity
+{
+    public class UserTask
+    {
+        [PrimaryKey, AutoIncrement]
+        public long Id { get; set; }
+        [NotNull]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [NotNull]
+        public DateTime StartsAt { get; set; }
+        [NotNull]
+        public DateTime EndsAt { get; set; }
+        [NotNull]
+        public long CategoryId { get; set; }
+    }
+}
