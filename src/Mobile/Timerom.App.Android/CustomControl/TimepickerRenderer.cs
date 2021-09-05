5b0d3aee80c3112637ddac2f64dc583d72ffe321
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-09-05 09:27:41 +0100

    style(Timepicker): Adding style to improve timepicker apparence

---
+++ src/Mobile/Timerom.App.Android/CustomControl/TimepickerRenderer.cs
@@ -0,0 +1,20 @@
+﻿using Timerom.App.Droid.CustomControl;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(TimePicker), typeof(TimepickerRenderer))]
+namespace Timerom.App.Droid.CustomControl
+{
+    public class TimepickerRenderer : TimePickerRenderer
+    {
+        public TimepickerRenderer(Android.Content.Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TimePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+                Control.SetBackgroundResource(0);
+        }
+    }
+}
