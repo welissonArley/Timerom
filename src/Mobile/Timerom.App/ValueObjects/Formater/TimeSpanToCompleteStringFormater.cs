9f9e04a469b45a0d178a597b297d67e37edd2043
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-09-05 09:29:48 +0100

    feat(Converter): Converte timespan to full string

---
+++ src/Mobile/Timerom.App/ValueObjects/Formater/TimeSpanToCompleteStringFormater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Timerom.App.ValueObjects.Formater
+{
+    public class TimeSpanToCompleteStringFormater
+    {
+        public string Format(TimeSpan time)
+        {
+            var response = "";
+
+            if (time.Hours == 1)
+                response = $"1 {ResourceText.TITLE_HOUR}";
+            else if (time.Hours > 1)
+                response = $"{time.Hours} {ResourceText.TITLE_HOURS}";
+
+            if (time.Minutes == 1)
+                response = $"{response} 1 {ResourceText.TITLE_MINUTE}";
+            else if (time.Minutes > 1)
+                response = $"{response} {time.Minutes} {ResourceText.TITLE_MINUTES}";
+
+            return string.IsNullOrWhiteSpace(response) ? "-" : response;
+        }
+    }
+}
