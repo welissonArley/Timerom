7397e30fa1119ebd8f603923236e9ac97f7222fc
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-09-06 08:55:10 +0100

    feat(TaskDetailsPage): now the user can change dates

--- src/Mobile/Timerom.App/ViewModels/Tasks/TaskDetailsViewModel.cs
+++ src/Mobile/Timerom.App/ViewModels/Tasks/TaskDetailsViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Threading.Tasks;
 using Timerom.App.Model;
 using Timerom.App.UseCase.UserTask.Interfaces;
+using Xamarin.CommunityToolkit.ObjectModel;
 
 namespace Timerom.App.ViewModels.Tasks
 {
@@ -13,22 +14,30 @@ namespace Timerom.App.ViewModels.Tasks
 
         public TasksDetailsModel Model { get; set; }
 
+        public IAsyncCommand<DateTime> DateChangedCommand { get; private set; }
+
         public TaskDetailsViewModel(Lazy<INavigationService> navigationService, Lazy<IGetAllUserTaskUseCase> useCase) : base(navigationService)
         {
             this.useCase = useCase;
+            DateChangedCommand = new AsyncCommand<DateTime>(GetUserTasks, onException: HandleException, allowsMultipleExecutions: false);
         }
 
-        public async Task InitializeAsync(INavigationParameters parameters)
+        private async Task GetUserTasks(DateTime date)
         {
-            var models = await _useCase.Execute(DateTime.Today);
+            var models = await _useCase.Execute(date);
 
             Model = new TasksDetailsModel
             {
-                Date = DateTime.Today,
+                Date = date,
                 Tasks = new System.Collections.ObjectModel.ObservableCollection<TaskModel>(models)
             };
 
             RaisePropertyChanged("Model");
+        }
+
+        public async Task InitializeAsync(INavigationParameters parameters)
+        {
+            await GetUserTasks(DateTime.Now);
         }
     }
 }
