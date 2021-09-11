f27d1a68cec4121962d2dbe0e87e1f401cbba8fc
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-09-11 16:04:51 +0100

    feat(AddUpdateTaskPage): use new way to delete a task

--- src/Mobile/Timerom.App/Views/Views/Tasks/AddUpdateTaskPage.xaml.cs
+++ src/Mobile/Timerom.App/Views/Views/Tasks/AddUpdateTaskPage.xaml.cs
@@ -1,10 +1,10 @@
-﻿using Xamarin.Forms;
+﻿using Timerom.App.CustomControl;
 using Xamarin.Forms.Xaml;
 
 namespace Timerom.App.Views.Views.Tasks
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AddUpdateTaskPage : ContentPage
+    public partial class AddUpdateTaskPage : AppContentPageCustomToolbar
     {
         public AddUpdateTaskPage()
         {
