b904cee324fe15840ed9d853b7e368eae7e13362
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-09-11 15:54:19 +0100

    feat(Toolbar): Custom page to show toolbar item

---
+++ src/Mobile/Timerom.App/CustomControl/AppContentPageCustomToolbar.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Timerom.App.CustomControl
+{
+    public class AppContentPageCustomToolbar : ContentPage
+    {
+        public AppToolbarItem CustomToolbar
+        {
+            get { return (AppToolbarItem)GetValue(CustomToolbarProperty); }
+            set { SetValue(CustomToolbarProperty, value); }
+        }
+        public static readonly BindableProperty CustomToolbarProperty = BindableProperty.Create(
+            propertyName: "CustomToolbar", returnType: typeof(AppToolbarItem), declaringType: typeof(AppContentPageCustomToolbar),
+            defaultValue: null,
+            defaultBindingMode: BindingMode.OneWay, propertyChanged: CustomToolbarChanged);
+
+        private static void CustomToolbarChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var item = (AppToolbarItem)newValue;
+            var component = (AppContentPageCustomToolbar)bindable;
+
+            item.PropertyChanged += (sender, e) =>
+            {
+                propertyChanged(sender, e, component, item);
+            };
+        }
+
+        private static void propertyChanged(object sender,
+            PropertyChangedEventArgs e, AppContentPageCustomToolbar appContent, AppToolbarItem item)
+        {
+            if(e.PropertyName.Equals("IsVisible"))
+                UpdateToolbar(appContent, item);
+        }
+
+        private static void UpdateToolbar(AppContentPageCustomToolbar component, AppToolbarItem item)
+        {
+            if (item.IsVisible)
+                component.ToolbarItems.Add(item);
+            else
+                component.ToolbarItems.Clear();
+        }
+    }
+}
---
+++ src/Mobile/Timerom.App/CustomControl/AppToolbarItem.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace Timerom.App.CustomControl
+{
+    public class AppToolbarItem : ToolbarItem
+    {
+        public bool IsVisible
+        {
+            get { return (bool)GetValue(IsVisibleProperty); }
+            set { SetValue(IsVisibleProperty, value); }
+        }
+        public static readonly BindableProperty IsVisibleProperty = BindableProperty.Create(
+            propertyName: "IsVisible", returnType: typeof(bool), declaringType: typeof(AppToolbarItem), defaultValue: false,
+            defaultBindingMode: BindingMode.OneWay, propertyChanged: null);
+    }
+}
