7ad5173b1bb0db67f3ee60364b823617eee0bbe3
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-09-14 09:07:47 +0100

    feat(FilterOption): adding filter option component

---
+++ src/Mobile/Timerom.App/Model/FilterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Timerom.App.Model
+{
+    public class FilterModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool Checked { get; set; }
+    }
+}
