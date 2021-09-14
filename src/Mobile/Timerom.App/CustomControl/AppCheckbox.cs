5fd171cdc305c0a8b9e31c62b4ab2671887c8ab7
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-09-14 09:12:23 +0100

    feat(Checkbox): adding custom checkbox to use command

---
+++ src/Mobile/Timerom.App/CustomControl/AppCheckbox.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace Timerom.App.CustomControl
+{
+    public partial class AppCheckbox : CheckBox
+    {
+        public IAsyncCommand CheckChangedCommand
+        {
+            get => (IAsyncCommand)GetValue(CheckChangedCommandProperty);
+            set => SetValue(CheckChangedCommandProperty, value);
+        }
+
+        public static readonly BindableProperty CheckChangedCommandProperty = BindableProperty.Create(
+            nameof(CheckChangedCommand),
+            typeof(IAsyncCommand),
+            typeof(AppCheckbox),
+            null
+        );
+
+        public AppCheckbox()
+        {
+            CheckedChanged += OnCheckChanged;
+        }
+
+        private void OnCheckChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (CheckChangedCommand?.CanExecute(e.Value) ?? false)
+            {
+                CheckChangedCommand.Execute(e.Value);
+            }
+        }
+    }
+}
