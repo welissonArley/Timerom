3c214beb2e047ac901e13a4f8226d14519dd1253
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-09-17 09:39:02 +0100

    feat(Andoid/iOs): Adding images to start and stop button and nuget packages to background service

--- src/Mobile/Timerom.App.Android/MainActivity.cs
+++ src/Mobile/Timerom.App.Android/MainActivity.cs
@@ -19,6 +19,7 @@ namespace Timerom.App.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Matcha.BackgroundService.Droid.BackgroundAggregator.Init(this);
 
             CachedImageRenderer.Init(true);
             var ignore = typeof(SvgCachedImage);
--- src/Mobile/Timerom.App.iOS/AppDelegate.cs
+++ src/Mobile/Timerom.App.iOS/AppDelegate.cs
@@ -14,6 +14,7 @@ namespace Timerom.App.iOS
             Rg.Plugins.Popup.Popup.Init();
 
             global::Xamarin.Forms.Forms.Init();
+            Matcha.BackgroundService.iOS.BackgroundAggregator.Init(this);
 
             CachedImageRenderer.Init();
             var ignore = typeof(SvgCachedImage);
