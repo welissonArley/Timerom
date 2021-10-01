d2ff87fa4aeb9dcd571a9ddb4bb469c88167c7c1
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-10-01 07:33:35 +0100

    feat(UserTaskDatabase): function to get user tasks in a date interval

--- src/Mobile/Timerom.App/Repository/UserTaskDatabase.cs
+++ src/Mobile/Timerom.App/Repository/UserTaskDatabase.cs
@@ -40,6 +40,13 @@ namespace Timerom.App.Repository
             return list.Where(c => c.StartsAt.Date == date.Date || c.EndsAt.Date == date.Date).OrderBy(c => c.StartsAt).ToList();
         }
 
+        public async Task<System.Collections.Generic.List<UserTask>> GetBeetwenDates(DateTime firstDate, DateTime secondDate)
+        {
+            var list = await _database.Table<UserTask>().ToListAsync();
+
+            return list.Where(c => c.StartsAt.Date >= firstDate.Date && c.EndsAt.Date <= secondDate.Date).ToList();
+        }
+
         public async Task<UserTask> GetLast(DateTime date)
         {
             var list = await _database.Table<UserTask>().ToListAsync();
