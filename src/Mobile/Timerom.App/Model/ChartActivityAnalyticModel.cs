e23da3a1fb0340d81729633907458d506ac5bd77
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-10-01 09:13:31 +0100

    feat(ChartActitivyAnalytic): Template em model to create chart to show user ActivityAnalytic Page

---
+++ src/Mobile/Timerom.App/Model/ChartActivityAnalyticModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Timerom.App.ValueObjects.Enuns;
+
+namespace Timerom.App.Model
+{
+    public class ChartActivityAnalyticModel
+    {
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public CategoryType Type { get; set; }
+    }
+}
---
+++ src/Mobile/Timerom.App/Views/Templates/Information/ChartActivityAnalyticTemplate.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using Timerom.App.Converter;
+using Timerom.App.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
+using Xamarin.Forms.Xaml;
+
+namespace Timerom.App.Views.Templates.Information
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChartActivityAnalyticTemplate : ContentView
+    {
+        public ObservableCollection<ChartActivityAnalyticModel> Values
+        {
+            get => (ObservableCollection<ChartActivityAnalyticModel>)GetValue(ValuesProperty);
+            set => SetValue(ValuesProperty, value);
+        }
+
+        public static readonly BindableProperty ValuesProperty = BindableProperty.Create(
+                                                        propertyName: "Values",
+                                                        returnType: typeof(ObservableCollection<ChartActivityAnalyticModel>),
+                                                        declaringType: typeof(ChartActivityAnalyticTemplate),
+                                                        defaultValue: null,
+                                                        defaultBindingMode: BindingMode.TwoWay,
+                                                        propertyChanged: ValuesChanged);
+
+        private static void ValuesChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var chartActivities = (ObservableCollection<ChartActivityAnalyticModel>)(newValue ?? oldValue);
+
+            if (chartActivities == null)
+                return;
+
+            var component = (ChartActivityAnalyticTemplate)bindable;
+
+            var today = DateTime.Today;
+            int index = 0;
+            var maxValue = chartActivities.Any() ? chartActivities.Max(c => c.Time.TotalMinutes) : 0;
+
+            component.GridContent.Children.Clear();
+
+            foreach (var activity in chartActivities)
+            {
+                component.GridContent.Children.Add(new Line
+                {
+                    BackgroundColor = (Color)new CategoryTypeColorConverter().Convert(activity.Type, typeof(Line), null, CultureInfo.CurrentCulture),
+                    Opacity = 1,
+                    HeightRequest = activity.Time.TotalMinutes == 0 ? .2 : activity.Time.TotalMinutes * 100 / maxValue,
+                    VerticalOptions = LayoutOptions.End
+                }, index, 0);
+                component.GridContent.Children.Add(new Label
+                {
+                    FontSize = 11,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                    Text = activity.Date.ToString("ddd"),
+                    Style = activity.Date.Day == today.Day ? (Style)Application.Current.Resources["LabelSemiBold"] : (Style)Application.Current.Resources["LabelBaseStyle"]
+                }, index, 1);
+                component.GridContent.Children.Add(new Label
+                {
+                    FontSize = 11,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                    Text = activity.Date.ToString("dd"),
+                    Style = activity.Date.Day == today.Day ? (Style)Application.Current.Resources["LabelSemiBold"] : (Style)Application.Current.Resources["LabelBaseStyle"]
+                }, index, 2);
+
+                index++;
+            }
+
+            for(var secondIndex = index; secondIndex < 7; secondIndex++)
+            {
+                component.GridContent.Children.Add(new Line
+                {
+                    BackgroundColor = Color.Black,
+                    Opacity = 1,
+                    HeightRequest = .2,
+                    VerticalOptions = LayoutOptions.End
+                }, secondIndex, 0);
+                component.GridContent.Children.Add(new Label
+                {
+                    FontSize = 11,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                    Text = "-"
+                }, secondIndex, 1);
+                component.GridContent.Children.Add(new Label
+                {
+                    FontSize = 11,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                    Text = "-"
+                }, secondIndex, 2);
+            }
+        }
+
+        public ChartActivityAnalyticTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
