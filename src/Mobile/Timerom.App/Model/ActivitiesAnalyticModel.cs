dd51f5e6fa1b6206c51810696cf2c7de8e0352f6
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-10-02 16:18:32 +0100

    feat(Model): model to use in page show activities from chart

---
+++ src/Mobile/Timerom.App/Model/ActivitiesAnalyticModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Timerom.App.Model
+{
+    public class ActivitiesAnalyticModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan Time { get; set; }
+    }
+}
---
+++ src/Mobile/Timerom.App/Model/ActivityAnalyticStatisticsModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Timerom.App.Model
+{
+    public class ActivityAnalyticStatisticsModel
+    {
+        public TaskAnalyticModel Productive { get; set; }
+        public TaskAnalyticModel Neutral { get; set; }
+        public TaskAnalyticModel Unproductive { get; set; }
+        public ObservableCollection<ActivitiesAnalyticModel> Activities { get; set; }
+    }
+}
