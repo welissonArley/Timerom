628906241b360419f9ef6c4c8acb0584dfe11fae
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-10-05 10:30:55 +0100

    feat(ParetoPrincipleResultPage): using model to show body information on the page

--- src/Mobile/Timerom.App/ViewModels/Reports/ParetoPrinciple/ParetoPrincipleResultViewModel.cs
+++ src/Mobile/Timerom.App/ViewModels/Reports/ParetoPrinciple/ParetoPrincipleResultViewModel.cs
@@ -21,7 +21,52 @@ namespace Timerom.App.ViewModels.Reports.ParetoPrinciple
             Model = new ParetoPrincipleModel
             {
                 PercentageCause = 21.2,
-                PercentageResult = 81.23
+                PercentageResult = 81.23,
+                Ranking = new System.Collections.ObjectModel.ObservableCollection<RankingParetoPrincipleModel>
+                {
+                    new RankingParetoPrincipleModel
+                    {
+                        AccumulatedPercentage = 46,
+                        IsPartOf80Percent = true,
+                        AmountOfTasks = 27,
+                        Index = 1,
+                        Percentage = 46,
+                        Time = new TimeSpan(hours: 0, minutes: 568, seconds: 0),
+                        Category = new Model.Category
+                        {
+                            Name = "Study",
+                            Type = ValueObjects.Enuns.CategoryType.Productive
+                        }
+                    },
+                    new RankingParetoPrincipleModel
+                    {
+                        AccumulatedPercentage = 82,
+                        IsPartOf80Percent = true,
+                        AmountOfTasks = 27,
+                        Index = 2,
+                        Percentage = 36,
+                        Time = new TimeSpan(hours: 0, minutes: 468, seconds: 0),
+                        Category = new Model.Category
+                        {
+                            Name = "Social Media",
+                            Type = ValueObjects.Enuns.CategoryType.Unproductive
+                        }
+                    },
+                    new RankingParetoPrincipleModel
+                    {
+                        AccumulatedPercentage = 83,
+                        IsPartOf80Percent = false,
+                        AmountOfTasks = 27,
+                        Index = 3,
+                        Percentage = 1,
+                        Time = new TimeSpan(hours: 0, minutes: 68, seconds: 0),
+                        Category = new Model.Category
+                        {
+                            Name = "Cooking",
+                            Type = ValueObjects.Enuns.CategoryType.Neutral
+                        }
+                    }
+                }
             };
 
             RaisePropertyChanged("Model");
--- src/Mobile/Timerom.App/Views/Templates/Information/ItemParetoPrincipleResultTemplate.xaml.cs
+++ src/Mobile/Timerom.App/Views/Templates/Information/ItemParetoPrincipleResultTemplate.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Timerom.App.Converter;
+using Timerom.App.Model;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Timerom.App.Views.Templates.Information
@@ -6,6 +9,38 @@ namespace Timerom.App.Views.Templates.Information
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ItemParetoPrincipleResultTemplate : ContentView
     {
+        public RankingParetoPrincipleModel Ranking
+        {
+            get => (RankingParetoPrincipleModel)GetValue(RankingProperty);
+            set => SetValue(RankingProperty, value);
+        }
+        public static readonly BindableProperty RankingProperty = BindableProperty.Create(
+                                                        propertyName: "Ranking",
+                                                        returnType: typeof(RankingParetoPrincipleModel),
+                                                        declaringType: typeof(ItemParetoPrincipleResultTemplate),
+                                                        defaultValue: null,
+                                                        defaultBindingMode: BindingMode.TwoWay,
+                                                        propertyChanged: RankingChanged);
+
+        private static void RankingChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var ranking = (RankingParetoPrincipleModel)(newValue ?? oldValue);
+
+            if (ranking == null)
+                return;
+
+            var component = (ItemParetoPrincipleResultTemplate)bindable;
+
+            component.EllipseIndex.Fill = (SolidColorBrush)new CategoryTypeColorConverter().Convert(ranking.Category.Type, typeof(Brush), null, null);
+            component.LabelIndex.Text = $"{(ranking.Index < 10 ? $"0{ranking.Index}" : $"{ranking.Index}")}";
+            component.LabelName.Text = ranking.Category.Name;
+            component.LabelAmountTasks.Text = string.Format(ResourceText.TITLE_TOTAL_OF_TASKS, ranking.AmountOfTasks);
+            component.LabelPercentage.Text = $"{(ranking.Percentage < 10 ? $"0{ranking.Percentage}" : $"{ranking.Percentage}")}%";
+            component.LabelHours.Text = $"{new HoursStringconverter().Convert(ranking.Time, typeof(Label), null, null)}";
+            component.LabelAccumulatedPercentage.Text = $"{ranking.AccumulatedPercentage}%";
+            component.IllustrationPartOf80Percent.IsVisible = ranking.IsPartOf80Percent;
+        }
+
         public ItemParetoPrincipleResultTemplate()
         {
             InitializeComponent();
