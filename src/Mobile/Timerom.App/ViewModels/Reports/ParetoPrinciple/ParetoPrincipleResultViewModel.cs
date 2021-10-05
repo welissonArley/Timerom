cf687937891c73b0b8bd08a04a30662897896a36
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-10-05 10:07:14 +0100

    feat(ParetoPrincipleResultPage): using model to show header information

--- src/Mobile/Timerom.App/ViewModels/Reports/ParetoPrinciple/ParetoPrincipleResultViewModel.cs
+++ src/Mobile/Timerom.App/ViewModels/Reports/ParetoPrinciple/ParetoPrincipleResultViewModel.cs
@@ -7,6 +7,7 @@ namespace Timerom.App.ViewModels.Reports.ParetoPrinciple
 {
     public class ParetoPrincipleResultViewModel : ViewModelBase, IInitializeAsync
     {
+        public string Period { get; set; }
         public ParetoPrincipleModel Model { get; set; }
 
         public ParetoPrincipleResultViewModel(Lazy<INavigationService> navigationService) : base(navigationService)
@@ -15,7 +16,15 @@ namespace Timerom.App.ViewModels.Reports.ParetoPrinciple
 
         public async Task InitializeAsync(INavigationParameters parameters)
         {
-            
+            Period = parameters.GetValue<string>("Period");
+
+            Model = new ParetoPrincipleModel
+            {
+                PercentageCause = 21.2,
+                PercentageResult = 81.23
+            };
+
+            RaisePropertyChanged("Model");
         }
     }
 }
