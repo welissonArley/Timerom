6a1e3f6c4d9e0afb807b13f0788fdaafd5c07afd
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-10-05 09:56:25 +0100

    feat(ParetoPrincipleResultPage): adding the model classes to use

---
+++ src/Mobile/Timerom.App/Model/ParetoPrincipleModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Timerom.App.Model
+{
+    public class ParetoPrincipleModel
+    {
+        public double PercentageResult { get; set; }
+        public double PercentageCause { get; set; }
+        public ObservableCollection<RankingParetoPrincipleModel> Ranking { get; set; }
+    }
+}
--- src/Mobile/Timerom.App/ViewModels/Reports/ParetoPrinciple/ParetoPrincipleResultViewModel.cs
+++ src/Mobile/Timerom.App/ViewModels/Reports/ParetoPrinciple/ParetoPrincipleResultViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using Prism.Navigation;
 using System;
 using System.Threading.Tasks;
+using Timerom.App.Model;
 
 namespace Timerom.App.ViewModels.Reports.ParetoPrinciple
 {
     public class ParetoPrincipleResultViewModel : ViewModelBase, IInitializeAsync
     {
+        public ParetoPrincipleModel Model { get; set; }
+
         public ParetoPrincipleResultViewModel(Lazy<INavigationService> navigationService) : base(navigationService)
         {
         }
