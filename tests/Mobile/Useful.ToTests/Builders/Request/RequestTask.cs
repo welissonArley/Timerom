720afc5660f71228245a3861929889fad29461d0
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-11-05 13:34:06 +0000

    feat(UnitTest): adding unit test to user task validation

---
+++ tests/Mobile/Useful.ToTests/Builders/Request/RequestTask.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using System;
+using Timerom.App.Model;
+
+namespace Useful.ToTests.Builders.Request
+{
+    public class RequestTask
+    {
+        private static RequestTask _instance;
+
+        public static RequestTask Instance()
+        {
+            _instance = new RequestTask();
+            return _instance;
+        }
+
+        public TaskModel Build()
+        {
+            return new Faker<TaskModel>()
+                .RuleFor(u => u.Title, (f) => f.Internet.UserName())
+                .RuleFor(u => u.StartsAt, () => DateTime.Now.AddHours(-2))
+                .RuleFor(u => u.EndsAt, () => DateTime.Now);
+        }
+    }
+}
