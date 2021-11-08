9cce06fd66821d66e3b6c862db8fe561029646ee
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-11-08 14:53:29 +0000

    feat(UnitTest): adding unit test to ActivityAnalyticBarSelectedViewModel

---
+++ tests/Mobile/Useful.ToTests/Builders/Request/RequestActivitiesAnalyticModel.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using Timerom.App.Model;
+using Timerom.App.ValueObjects.Enuns;
+
+namespace Useful.ToTests.Builders.Request
+{
+    public class RequestActivitiesAnalyticModel
+    {
+        private static RequestActivitiesAnalyticModel _instance;
+
+        public static RequestActivitiesAnalyticModel Instance()
+        {
+            _instance = new RequestActivitiesAnalyticModel();
+            return _instance;
+        }
+
+        public ActivitiesAnalyticModel Build()
+        {
+            return new Faker<ActivitiesAnalyticModel>()
+                .RuleFor(u => u.Name, (f) => f.Internet.UserName())
+                .RuleFor(u => u.Time, () => new System.TimeSpan(1,1,1))
+                .RuleFor(u => u.Type, (f) => f.PickRandom<CategoryType>())
+                .RuleFor(u => u.Percentage, (f) => f.Random.Double(1, 100));
+        }
+    }
+}
---
+++ tests/Mobile/ViewModels.Test/Reports/ActivityAnalytic/ActivityAnalyticBarSeletedCategoryViewModelTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Prism.Navigation;
+using System;
+using System.Threading.Tasks;
+using Timerom.App.UseCase.Reports.ActivityAnalytic.Interfaces;
+using Timerom.App.ViewModels.Reports.ActivityAnalytic;
+using Useful.ToTests.Builders.Navigation;
+using Useful.ToTests.Builders.Request;
+using Useful.ToTests.Builders.UseCase;
+using Xunit;
+
+namespace ViewModels.Test.Reports.ActivityAnalytic
+{
+    public class ActivityAnalyticBarSeletedCategoryViewModelTest
+    {
+        [Fact]
+        public void Validade_Sucess()
+        {
+            var navigation = new Lazy<INavigationService>(() => INavigationServiceBuilder.Instance().Build());
+
+            var useCase = new Lazy<IActivityAnalyticDetailsSubCategoryUseCase>(() => ActivityAnalyticDetailsSubCategoryUseCaseBuilder.Instance().Build());
+
+            var viewModel = new ActivityAnalyticBarSeletedCategoryViewModel(navigation, useCase);
+
+            viewModel.Activities.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task Validade_Sucess_Initialize()
+        {
+            var navigation = new Lazy<INavigationService>(() => INavigationServiceBuilder.Instance().Build());
+
+            var useCase = new Lazy<IActivityAnalyticDetailsSubCategoryUseCase>(() => ActivityAnalyticDetailsSubCategoryUseCaseBuilder.Instance().Execute().Build());
+
+            var viewModel = new ActivityAnalyticBarSeletedCategoryViewModel(navigation, useCase);
+
+            var parameters = INavigationParametersBuilder.Instance()
+                .Parameter("Date", DateTime.Today)
+                .Parameter("ActivitiesAnalytic", RequestActivitiesAnalyticModel.Instance().Build())
+                .Build();
+
+            Func<Task> action = async () => await viewModel.InitializeAsync(parameters);
+
+            await action.Should().NotThrowAsync();
+
+            viewModel.Description.Should().NotBeNullOrWhiteSpace();
+            viewModel.Activities.Should().NotBeNullOrEmpty();
+        }
+    }
+}
