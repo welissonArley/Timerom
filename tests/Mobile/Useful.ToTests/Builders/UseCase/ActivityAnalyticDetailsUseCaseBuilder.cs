bea68ff3b3fd4b7faff71633c07172bfae0561c5
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-11-08 15:14:51 +0000

    feat(UnitTest): adding unit test to ActivityAnalyticBarSelectedViewModel

---
+++ tests/Mobile/Useful.ToTests/Builders/UseCase/ActivityAnalyticDetailsUseCaseBuilder.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using System;
+using Timerom.App.UseCase.Reports.ActivityAnalytic.Interfaces;
+using Useful.ToTests.Builders.Request;
+
+namespace Useful.ToTests.Builders.UseCase
+{
+    public class ActivityAnalyticDetailsUseCaseBuilder
+    {
+        private static ActivityAnalyticDetailsUseCaseBuilder _instance;
+        private readonly Mock<IActivityAnalyticDetailsUseCase> _repository;
+
+        private ActivityAnalyticDetailsUseCaseBuilder()
+        {
+            if (_repository == null)
+                _repository = new Mock<IActivityAnalyticDetailsUseCase>();
+        }
+
+        public static ActivityAnalyticDetailsUseCaseBuilder Instance()
+        {
+            _instance = new ActivityAnalyticDetailsUseCaseBuilder();
+            return _instance;
+        }
+
+        public ActivityAnalyticDetailsUseCaseBuilder Execute()
+        {
+            _repository.Setup(c => c.Execute(It.IsAny<DateTime>())).ReturnsAsync(RequestActivityAnalyticStatisticsModel.Instance().Build());
+            return this;
+        }
+
+        public IActivityAnalyticDetailsUseCase Build()
+        {
+            return _repository.Object;
+        }
+    }
+}
