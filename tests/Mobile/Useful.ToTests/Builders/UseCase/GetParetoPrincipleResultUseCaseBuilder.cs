1d602dd684049ab1dbaf350806166137aa7efec8
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-11-15 09:21:55 +0000

    feat(UnitTest): adding unit test to Pareto Principle Result view model

---
+++ tests/Mobile/Useful.ToTests/Builders/UseCase/GetParetoPrincipleResultUseCaseBuilder.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using Timerom.App.Model;
+using Timerom.App.UseCase.Reports.ActivityAnalytic.Interfaces;
+using Timerom.App.ValueObjects.Dto;
+
+namespace Useful.ToTests.Builders.UseCase
+{
+    public class GetParetoPrincipleResultUseCaseBuilder
+    {
+        private static GetParetoPrincipleResultUseCaseBuilder _instance;
+        private readonly Mock<IGetParetoPrincipleResultUseCase> _repository;
+
+        private GetParetoPrincipleResultUseCaseBuilder()
+        {
+            if (_repository == null)
+                _repository = new Mock<IGetParetoPrincipleResultUseCase>();
+        }
+
+        public static GetParetoPrincipleResultUseCaseBuilder Instance()
+        {
+            _instance = new GetParetoPrincipleResultUseCaseBuilder();
+            return _instance;
+        }
+
+        public GetParetoPrincipleResultUseCaseBuilder Execute()
+        {
+            _repository.Setup(c => c.Execute(It.IsAny<ParetoPrincipleFilter>())).ReturnsAsync(new ParetoPrincipleModel());
+            return this;
+        }
+
+        public IGetParetoPrincipleResultUseCase Build()
+        {
+            return _repository.Object;
+        }
+    }
+}
