4ab536515b1c9e7a453e150eca357f5442b53dc5
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-11-15 16:36:52 +0000

    feat(UnitTest): adding unit tests to FloatActionUserTaskViewModel

---
+++ tests/Mobile/ViewModels.Test/Modal/MenuOptions/FloatActionUserTaskViewModelTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Prism.Navigation;
+using System;
+using Timerom.App.ViewModels.Modal.MenuOptions;
+using Useful.ToTests.Builders.Navigation;
+using Xunit;
+
+namespace ViewModels.Test.Modal.MenuOptions
+{
+    public class FloatActionUserTaskViewModelTest
+    {
+        [Fact]
+        public void Validade_Sucess()
+        {
+            var navigation = new Lazy<INavigationService>(() => INavigationServiceBuilder.Instance().Build());
+
+            var viewModel = new FloatActionUserTaskViewModel(navigation);
+
+            viewModel.AddUserTaskCommand.Should().NotBeNull();
+            viewModel.StartUserTaskCommand.Should().NotBeNull();
+        }
+    }
+}
