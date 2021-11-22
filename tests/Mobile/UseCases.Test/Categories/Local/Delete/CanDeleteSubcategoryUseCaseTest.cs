6453aed6372c86ab97a4847ebdb7e7aaf88ed086
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-11-22 09:34:50 +0000

    feat(UnitTest): adding unit test to usecase CanDeleteSubcategory

---
+++ tests/Mobile/UseCases.Test/Categories/Local/Delete/CanDeleteSubcategoryUseCaseTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using System;
+using System.Threading.Tasks;
+using Timerom.App.Repository.Interface;
+using Timerom.App.UseCase.Categories.Local.Delete;
+using Timerom.Exception;
+using Timerom.Exception.ExceptionBase;
+using Useful.ToTests.Builders.Repositories;
+using Useful.ToTests.Builders.Request;
+using Xunit;
+
+namespace UseCases.Test.Categories.Local.Delete
+{
+    public class CanDeleteSubcategoryUseCaseTest
+    {
+        [Fact]
+        public async Task Sucess()
+        {
+            var repository = new Lazy<IUserTaskReadOnlyRepository>(() => UserTaskReadOnlyRepositoryBuilder.Instance().Build());
+
+            var useCase = new CanDeleteSubcategoryUseCase(repository);
+
+            var category = RequestCategory.Instance().Build();
+            Func<Task> action = async () => await useCase.Execute(category);
+
+            await action.Should().NotThrowAsync();
+        }
+
+        [Fact]
+        public async Task Cannot_Delete()
+        {
+            var repository = new Lazy<IUserTaskReadOnlyRepository>(() => UserTaskReadOnlyRepositoryBuilder.Instance().ExistTaskForSubcategory().Build());
+
+            var useCase = new CanDeleteSubcategoryUseCase(repository);
+
+            var category = RequestCategory.Instance().Build();
+            Func<Task> action = async () => await useCase.Execute(category);
+
+            await action.Should().ThrowAsync<ErrorOnValidationException>()
+                .Where(c => c.ErrorMensages.Count == 1 && c.ErrorMensages.Contains(ResourceTextException.THERE_IS_TASK_ASSOCIATED_SUBCATEGORY));
+        }
+    }
+}
---
+++ tests/Mobile/Useful.ToTests/Builders/Repositories/UserTaskReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using Timerom.App.Repository.Interface;
+
+namespace Useful.ToTests.Builders.Repositories
+{
+    public class UserTaskReadOnlyRepositoryBuilder
+    {
+        private static UserTaskReadOnlyRepositoryBuilder _instance;
+        private readonly Mock<IUserTaskReadOnlyRepository> _repository;
+
+        private UserTaskReadOnlyRepositoryBuilder()
+        {
+            if (_repository == null)
+                _repository = new Mock<IUserTaskReadOnlyRepository>();
+        }
+
+        public static UserTaskReadOnlyRepositoryBuilder Instance()
+        {
+            _instance = new UserTaskReadOnlyRepositoryBuilder();
+            return _instance;
+        }
+
+        public UserTaskReadOnlyRepositoryBuilder ExistTaskForSubcategory()
+        {
+            _repository.Setup(c => c.ExistTaskForSubcategory(It.IsAny<long>())).ReturnsAsync(true);
+            return this;
+        }
+
+        public IUserTaskReadOnlyRepository Build()
+        {
+            return _repository.Object;
+        }
+    }
+}
