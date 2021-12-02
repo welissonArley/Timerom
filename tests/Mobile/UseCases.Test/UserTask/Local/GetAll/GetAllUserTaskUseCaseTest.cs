cff8a2fec2ea604258be82283bdf7081123422b1
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-12-02 11:00:01 +0000

    feat(UnitTest): unit test to usecase to get all user task with filter

--- tests/Mobile/UseCases.Test/UserTask/Local/GetAll/GetAllUserTaskUseCaseTest.cs
+++ tests/Mobile/UseCases.Test/UserTask/Local/GetAll/GetAllUserTaskUseCaseTest.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Timerom.App.Model;
 using Timerom.App.Repository.Interface;
 using Timerom.App.UseCase.UserTask.Local.GetAll;
 using Useful.ToTests.Builders.Repositories;
@@ -20,9 +21,31 @@ namespace UseCases.Test.UserTask.Local.GetAll
 
             var useCase = new GetAllUserTaskUseCase(repositoryCategoryRead, repositoryUserTaskRead);
 
-            Func<Task> action = async () => await useCase.Execute(DateTime.Now, new List<long>());
+            TasksDetailsModel response = null;
+            Func<Task> action = async () => response = await useCase.Execute(DateTime.Now, new List<long>());
 
             await action.Should().NotThrowAsync();
+
+            response.Tasks.Should().HaveCountGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task Sucess_Filter()
+        {
+            var categoryRepositoryInstance = CategoryReadOnlyRepositoryBuilder.Instance();
+            var repositoryUserTaskRead = new Lazy<IUserTaskReadOnlyRepository>(() => UserTaskReadOnlyRepositoryBuilder.Instance().GetAll(categoryRepositoryInstance).Build());
+            var repositoryCategoryRead = new Lazy<ICategoryReadOnlyRepository>(() => categoryRepositoryInstance.Build());
+
+            var useCase = new GetAllUserTaskUseCase(repositoryCategoryRead, repositoryUserTaskRead);
+
+            TasksDetailsModel response = null;
+
+            var categoryIdToFilter = 2;
+            Func<Task> action = async () => response = await useCase.Execute(DateTime.Now, new List<long> { categoryIdToFilter });
+
+            await action.Should().NotThrowAsync();
+
+            response.Tasks.Should().HaveCount(1);
         }
     }
 }
