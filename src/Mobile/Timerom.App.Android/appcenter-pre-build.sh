a97d6f0f876de840889c00d92485583d7f5dcaa7
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-12-07 21:33:05 +0000

    refact(appcenter-pre-build): appcenter uses a mac to build, so it is necessary use argument to flag -i

--- src/Mobile/Timerom.App.Android/appcenter-pre-build.sh
+++ src/Mobile/Timerom.App.Android/appcenter-pre-build.sh
@@ -7,7 +7,7 @@ echo $APP_CONSTANT_FILE
 if [ -e "$APP_CONSTANT_FILE" ]
 then
     echo "Updating AppCenter secret"
-    sed -i 's/Android_AppCenterSecret = "SecretHere"/Android_AppCenterSecret = "'$APPCENTERSECRET'"' $APP_CONSTANT_FILE
+    sed -i '' 's/Android_AppCenterSecret = "SecretHere"/Android_AppCenterSecret = "'$APPCENTERSECRET'"' $APP_CONSTANT_FILE
 
     echo "File content:"
     cat $APP_CONSTANT_FILE
