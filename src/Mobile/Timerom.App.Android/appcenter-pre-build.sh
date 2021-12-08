3a98a83ca69425670dc2da49aac367985632c99b
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-12-08 06:34:47 +0000

    refact(appcenter-pre-build): script to run on pre-build is ready

--- src/Mobile/Timerom.App.Android/appcenter-pre-build.sh
+++ src/Mobile/Timerom.App.Android/appcenter-pre-build.sh
@@ -2,11 +2,6 @@
 
 APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/src/Mobile/Timerom.App/AppConstant.cs
 
-echo "Variabel bloqueada"
-echo $APPCENTERSECRET
-echo "Variavel visivel"
-echo $APPTEST
-
 if [ -e "$APP_CONSTANT_FILE" ]
 then
     sed -i '' 's#Android_AppCenterSecret = "SecretHere"#Android_AppCenterSecret = "'$APPCENTERSECRET'"#' $APP_CONSTANT_FILE
