4ce231c9c4b2849129f8df9f85beb707c385ea65
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-12-08 06:19:57 +0000

    test(appcenter-pre-build): testing script in environment

--- src/Mobile/Timerom.App.Android/appcenter-pre-build.sh
+++ src/Mobile/Timerom.App.Android/appcenter-pre-build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/src/Mobile/Timerom.App/AppConstant.cs
-echo "To aqui"
-echo $APP_CONSTANT_FILE
+
+echo "Variabel bloqueada"
+echo $APPCENTERSECRET
+echo "Variavel visivel"
+echo $APPTEST
 
 if [ -e "$APP_CONSTANT_FILE" ]
 then
-    echo "Updating AppCenter secret"
     sed -i '' 's#Android_AppCenterSecret = "SecretHere"#Android_AppCenterSecret = "'$APPCENTERSECRET'"#' $APP_CONSTANT_FILE
-
-    echo "File content:"
-    cat $APP_CONSTANT_FILE
 fi
