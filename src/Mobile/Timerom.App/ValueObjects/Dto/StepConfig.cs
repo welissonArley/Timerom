d134e208d12876693c53ce6e445fe42eb72a334b
AzureAD\WelissonVilaçaSantos <welisson.santos@vortal.biz>
2021-12-17 17:19:48 +0000

    style: using numered step to improve ui

---
+++ src/Mobile/Timerom.App/ValueObjects/Dto/StepConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace Timerom.App.ValueObjects.Dto
+{
+    public class StepConfig
+    {
+        public uint TotalNumberOfSteps { get; set; }
+        public uint StepsCompleted { get; set; }
+    }
+}
---
+++ src/Mobile/Timerom.App/Views/Templates/Information/StepProgressTemplate.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Timerom.App.ValueObjects.Dto;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Timerom.App.Views.Templates.Information
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StepProgressTemplate : ContentView
+    {
+        public StepConfig StepConfig
+        {
+            get => (StepConfig)GetValue(StepConfigProperty);
+            set => SetValue(StepConfigProperty, value);
+        }
+        public static readonly BindableProperty StepConfigProperty = BindableProperty.Create(
+                                                        propertyName: "StepConfig",
+                                                        returnType: typeof(StepConfig),
+                                                        declaringType: typeof(StepProgressTemplate),
+                                                        defaultValue: null,
+                                                        defaultBindingMode: BindingMode.OneWay,
+                                                        propertyChanged: StepConfigChanged);
+
+        private static void StepConfigChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var configObject = oldValue != null && newValue == null ? oldValue : newValue;
+
+            var component = (StepProgressTemplate)bindable;
+            var stepConfig = (StepConfig)configObject;
+
+            for (var index = 1; index <= stepConfig.TotalNumberOfSteps; index++)
+            {
+                if(index <= stepConfig.StepsCompleted)
+                    component.StepContent.Children.Add(CompleteStep(index));
+                else
+                    component.StepContent.Children.Add(NextStep(index));
+
+                if (index < stepConfig.TotalNumberOfSteps)
+                    component.StepContent.Children.Add(LineStep());
+            }
+        }
+
+        private static AvatarView CompleteStep(int index)
+        {
+            return new AvatarView
+            {
+                TextColor = Application.Current.RequestedTheme == OSAppTheme.Light ? Color.White : Color.Black,
+                Color = Application.Current.RequestedTheme == OSAppTheme.Light ? Color.Black : Color.White,
+                Size = 16,
+                Text = $"{index}"
+            };
+        }
+
+        private static AvatarView NextStep(int index)
+        {
+            return new AvatarView
+            {
+                TextColor = Application.Current.RequestedTheme == OSAppTheme.Light ? Color.Black : Color.White,
+                Color = Application.Current.RequestedTheme == OSAppTheme.Light ? Color.White : Color.Black,
+                BorderColor = Application.Current.RequestedTheme == OSAppTheme.Light ? Color.Black : Color.White,
+                Size = 16,
+                Text = $"{index}"
+            };
+        }
+
+        private static BoxView LineStep()
+        {
+            return new BoxView
+            {
+                Color = Application.Current.RequestedTheme == OSAppTheme.Light ? Color.Black : Color.White,
+                HeightRequest = 2,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                WidthRequest = 30
+            };
+        }
+
+        public StepProgressTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
